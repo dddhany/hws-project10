bfec8f5a06cf5290431764abcd0f91c37c2c94c9
Dhany Basuki <dddhany+github@gmail.com>
2016-03-19 21:43:30 +0800

    designing uicollectionview cell

---
+++ project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by Dhany Basuki on 19/3/16.
+//  Copyright © 2016 Dhany. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
